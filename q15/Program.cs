87999a376805d8ce789363c34ec931777a5ec2d9
Your Name <you@example.com>
2022-10-10 14:01:57 +0300

    определение выходного или будний день

---
+++ q15/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int startWeek =1;
+int finishWeek =7;
+
+Console.WriteLine("Введите номер дня недели:");
+
+int numberDayWeek = int.Parse(Console.ReadLine());
+if(numberDayWeek >=6){
+    Console.WriteLine("Данный день выходной");
+}
+else{
+    Console.WriteLine("Данный день будний");
+}
