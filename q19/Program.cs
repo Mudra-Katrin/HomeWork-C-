5456bdfe99af1a61ee514438647af60921dd2b2d
Your Name <you@example.com>
2022-10-12 16:27:31 +0300

    попытка

---
+++ q19/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 19
+//Напишите программу,которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом.
+
+bool IsPalindrome(int number){
+    int reverseNumber = ReverseNumber(number);
+    bool isEqual = reverseNumber == number;
+    return isEqual;
+}
+
+int  ReverseNumber(int number){
+    int reverseNumber = 0;
+    while(number > 0){
+         reverseNumber =  reverseNumber*10 + number %10;
+         number /=10;
+
+    }
+    return reverseNumber;
+}
+
+bool IsNumberVaid(int number){
+    if(number < 10000 || number > 10000)
+          return false;
+
+    return true;      
+}
+
+int Promt(){
+    Console.WriteLine("Введите 5-значное число!");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = Promt();
+if (IsNumberVaid(number)){
+    if(IsPalindrome(number))
+        Console.WriteLine("Yes");
+    else
+       Console.WriteLine("No");     
+}
+else{
+    Console.WriteLine("Error");
+}
+
+Console.WriteLine("Hello, World!");
