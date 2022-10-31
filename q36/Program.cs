93c778e1522d5158238e300184858fcbc5bc85aa
Your Name <you@example.com>
2022-10-31 22:08:57 +0200

    вывести сумму значений стоящих на нечетных позициях

--- q36/Program.cs
+++ q36/Program.cs
@@ -2,41 +2,56 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 
 
-int arrayLength =  GetIntInput("Введите длинну массива");
-int minValue =  GetIntInput("Введите минимальное значение массива");
-int maxValue =  GetIntInput("Введите максимальное значение массива");
+int arrayLength =  GetPositiveInput("Введите длинну массива :");
+int minValue =  GetIntInput("Введите минимальное значение массива :");
+int maxValue =  GetIntInput("Введите максимальное значение массива :");
 
 int[] array = MakeRandomArray(arrayLength, minValue, maxValue);
+PrintSummOddIndexArray(array);
 
-int[] MakeRandomArray(int arrayLength, int minValue, int maxValue)
+void PrintSummOddIndexArray(int[]inputArray)
 {
-    int[] madeArray = new int[arrayLength];
-    for (int i = 0; i < arrayLength; i++)
+    int oddSum=0;
+        for (int i = 1; i < inputArray.Length; i+=2)
     {
-        madeArray[i] = new Random().Next(minValue, maxValue+1);
-    }
+        oddSum += inputArray[i];
+    }            
+string inputString = string.Join(",", inputArray);
+Console.WriteLine("\nArray[" + inputString + "]\noddSum=" + oddSum);  
+}
 
+int[]MakeRandomArray(int arrayLength, int minValue, int maxValue)
+{
+    int[]madeArray=new int [arrayLength];
+    for(int i=0; i<arrayLength;i++)
+    {
+        madeArray[i]=new Random().Next(minValue, maxValue+1);
+    }
     return madeArray;
 }
 
+int GetPositiveInput(string text)
+{
+    Console.Write(text);
+    int num;
+    while (! int.TryParse(Console.ReadLine(),out num) ^ num<0)
+    {
+Console.Write("Ошибка\n" + text);
+    }
+    return num;
+}
 int GetIntInput(string text)
 {
     Console.Write(text);
-
-    int num = int.Parse(Console.ReadLine()!);
-    
-    return num;
-}
-
-    int input = MakeRandomArray(arrayLength, minValue, maxValue);
-    int oddSum = 0;
-    for (int i=1; i<arrayLength; i+2)
+    int num;
+    while(!int.TryParse(Console.ReadLine(),out num))
     {
-        oddSum += input [i];
+        Console.Write("Ошибка\n"+text);
     }
+return num;
+}
 //берем значение  1го индекса (т.к. он первый нечетный) , если оно меньше длины массива
 //суммируем его  и далее переходим к значению индекса увеличивая его на 2
-string inputString = string.Join(",", input);
-Console.WriteLine("Array=" + inputString + "oddSum=" + oddSum);
+
 
 
